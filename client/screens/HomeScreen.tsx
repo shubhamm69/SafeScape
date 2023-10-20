2cad8b7e7ecc0240cab8bab11a74b3c8c7356467
shubhamm69 <shubham212a@gmail.com>
2023-10-20 18:12:43 +0530

    homescreen modified

--- client/screens/HomeScreen.tsx
+++ client/screens/HomeScreen.tsx
@@ -1,20 +1,67 @@
-// screens/HomeScreen.js
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import { NavigationProp } from "@react-navigation/native";
+
+interface MenuItem {
+  id: number;
+  title: string;
+  subtitle: string;
+  image: any;
+}
 
 interface HomeScreenProps {
   navigation: NavigationProp<any>;
 }
 
+const menuItems: MenuItem[] = [
+  {
+    id: 1,
+    title: "Discover",
+    subtitle: "Explore the world",
+    image: require("../images/discover.png"),
+  },
+  {
+    id: 2,
+    title: "Resources",
+    subtitle: "Get helpful resources",
+    image: require("../images/resources.png"),
+  },
+  // {
+  //   id: 3,
+  //   title: "Profile",
+  //   subtitle: "Manage your profile",
+  //   image: require("../images/profile.png"),
+  // },
+];
+
 function HomeScreen({ navigation }: HomeScreenProps) {
+  const renderItem = ({ item }: { item: MenuItem }) => (
+    <TouchableOpacity
+      style={styles.menuItem}
+      onPress={() => navigation.navigate(item.title)}
+    >
+      <Image source={item.image} style={styles.menuItemImage} />
+      <Text style={styles.menuItemTitle}>{item.title}</Text>
+      <Text style={styles.menuItemSubtitle}>{item.subtitle}</Text>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("Resources")}>
-        <Text>Go to Resources</Text>
-      </TouchableOpacity>
+      <Text style={styles.title}>Welcome to SafeScape</Text>
+      <FlatList
+        data={menuItems}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+      />
     </View>
   );
 }
@@ -24,6 +71,40 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#f0f0f0",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  menuItem: {
+    width: 150,
+    height: 230,
+    backgroundColor: "#ffffff",
+    borderRadius: 10,
+    margin: 10,
+    padding: 10,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 3,
+  },
+  menuItemImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+  menuItemTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 5,
+  },
+  menuItemSubtitle: {
+    fontSize: 14,
+    textAlign: "center",
   },
 });
 
