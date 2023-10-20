37458f83b4f8c287635dacc7ee0e207fe6588525
shubhamm69 <shubham212a@gmail.com>
2023-10-20 17:41:34 +0530

    implemnted bottom stack navigation

--- client/navigation/TabNavigator.tsx
+++ client/navigation/TabNavigator.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StackNavigator } from "./StackNavigator";
+import { MaterialIcons } from "@expo/vector-icons";
 import ResourcesScreen from "../screens/ResourcesScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 import AlertScreen from "../screens/AlertScreen";
@@ -9,11 +10,47 @@ const Tab = createBottomTabNavigator();
 
 export function TabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={StackNavigator} />
-      <Tab.Screen name="Resources" component={ResourcesScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Alert" component={AlertScreen} />
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen
+        name="Home"
+        component={StackNavigator}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Resources"
+        component={ResourcesScreen}
+        options={{
+          tabBarLabel: "Resources",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="library-books" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="person" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Alert"
+        component={AlertScreen}
+        options={{
+          tabBarLabel: "Alert",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="add-alert" color={color} size={26} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
